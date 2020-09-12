400ef9c781ca4dc66c48fd4aeb54e036652e42e5
Thunder9527 <69445551+Thunder9527@users.noreply.github.com>
2020-09-12 17:30:17 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config){
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:8000/api/z8',
+    baseURL: 'codewhy',
     timeout: 5000
   })
 
